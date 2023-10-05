4df2a21c698e7a49f1a8e29e615b80aaa19ec426
Hasan-Rifat <developerhr000@gmail.com>
2023-10-05 19:56:32 +0600

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#0970cd",
+      secondary: "#0198e9",
+      tertiary: "#333333",
+      gd: {
+        // Define the gradient colors here
+        start: "#FF7A00",
+        end: "#FFB800",
+      },
+      red: "#FF5555",
+      stoke: "#e9e9e9",
+      bg: "#fafafa",
+    },
+  },
+  plugins: [],
+};
+export default config;
